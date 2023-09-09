c1b226d5c024c05e03d9478342f702f497a7afef
Drako <abhishek491981@gmail.com>
2023-09-09 18:22:49 +0530

    api concealed

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,6 @@
 const apiKey = '8af7d19ee89a4b4185bb4c0e4c3c339b';
+const baseUrl = 'https://newsapi.org';
 const String fetchingUrl =
-    'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=$apiKey';
+    '$baseUrl/v2/top-headlines?country=us&category=business&apiKey=$apiKey';
 const String noImage =
     "https://accordelectrotechnics.in/img/product/no-preview/no-preview.png";
